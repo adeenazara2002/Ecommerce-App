c8e102b8deb43e3ab23a6f38423eecf9fe61a058
adeenazara2002 <adinazara2001@gmail.com>
2024-08-15 23:52:43 +0500

    ecommerce app

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerceapp/widgets/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,42 +14,56 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: // Main container
-            Container(
-          height: MediaQuery.of(context).size.height,
+        child: Container(
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
             color: Color.fromRGBO(255, 255, 255, 1),
-            // borderRadius: BorderRadius.only(
-            //   bottomLeft: Radius.circular(40),
-            //   bottomRight: Radius.circular(40),
-            // ),
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(
+              // Navbar
+              Container(
+                padding: EdgeInsets.only(
+                    top: 80, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceBetween, 
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: AppColors.homeColor,
+                      size: 30,
+                    ),
+                    Text(
+                      'Home',
+                      style: TextStyle(
+                        color: AppColors.homeColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Icon(
+                      Icons.search_sharp,
+                      color: AppColors.homeColor,
+                      size: 30,
+                    ),
+                  ],
+                ),
+              ),
+
+              // ==== PURPLE CONTAINER ====
+
+              Container(
+                margin:
+                    EdgeInsets.only(top: 30), 
                 height: 150,
-              ),
-              // === Image Container ===
-              Container(
-                padding: EdgeInsets.only(left: 40),
-                child: Image.asset('assets/images/Logo.png'),
-              ),
-
-              // === text container ===
-              SizedBox(
-                height: 10,
-              ),
-
-              Container(
-                padding: EdgeInsets.only(left: 40),
-                child: Text(
-                  'Full Name',
-                  style: TextStyle(
-                      fontSize: 14.95,
-                      fontWeight: FontWeight.w400,
-                      color: AppColors.textColor),
+                width: 360,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(170, 20, 240, 1),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(
+                    color: Color.fromRGBO(170, 20, 240, 1),
+                  ),
                 ),
               ),
             ],
