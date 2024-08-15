dd943608867dde33464b3d66ccbdb3a73fe9cc37
adeenazara2002 <adinazara2001@gmail.com>
2024-08-16 02:01:55 +0500

    ecommerce app

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -17,7 +17,7 @@ class _HomeState extends State<Home> {
         child: Container(
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            color: Color.fromRGBO(255, 255, 255, 1),
+            color: Color.fromRGBO(245, 245, 245, 1),
           ),
           child: Column(
             children: [
@@ -314,6 +314,223 @@ class _HomeState extends State<Home> {
                       ),
                     ],
                   )
+                ],
+              ),
+
+              // column products started
+              // container 1
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 20)),
+                      Container(
+                        height: 250,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Color.fromRGBO(255, 255, 255, 1),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Padding(padding: EdgeInsets.only(left: 110)),
+                                Icon(
+                                  Icons.favorite,
+                                  color: Color.fromRGBO(206, 206, 206, 1),
+                                  size: 30,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/bag.png',
+                                  width: 130,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                        padding:
+                                            EdgeInsets.only(left: 10, top: 30)),
+                                    Text(
+                                      'Leather Women Bag',
+                                      style: TextStyle(
+                                          color: Color.fromRGBO(54, 52, 49, 1),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(padding: EdgeInsets.only(left: 10)),
+                                    Icon(
+                                      Icons.star,
+                                      color: Color.fromRGBO(255, 176, 1, 1),
+                                      size: 20,
+                                    ),
+                                    Padding(padding: EdgeInsets.only(left: 10)),
+                                    Text(
+                                      '(715 reviews)',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(154, 153, 152, 1),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w500),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(padding: EdgeInsets.only(left: 10)),
+                                    Text(
+                                      '\$290.00',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromRGBO(170, 20, 240, 1),
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      // container 2
+
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Padding(padding: EdgeInsets.only(left: 20)),
+                              Container(
+                                height: 250,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(255, 255, 255, 1),
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                  ),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Padding(
+                                            padding:
+                                                EdgeInsets.only(left: 110)),
+                                        Icon(
+                                          Icons.favorite,
+                                          color:
+                                              Color.fromRGBO(206, 206, 206, 1),
+                                          size: 30,
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Image.asset(
+                                          'assets/images/headphone.png',
+                                          width: 130,
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Padding(
+                                                padding: EdgeInsets.only(
+                                                    left: 10, top: 30)),
+                                            Text(
+                                              'Leather Women Bag',
+                                              style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      54, 52, 49, 1),
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w500),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 10)),
+                                            Icon(
+                                              Icons.star,
+                                              color: Color.fromRGBO(
+                                                  255, 176, 1, 1),
+                                              size: 20,
+                                            ),
+                                            Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 10)),
+                                            Text(
+                                              '(715 reviews)',
+                                              style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      154, 153, 152, 1),
+                                                  fontSize: 12,
+                                                  fontWeight: FontWeight.w500),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 10)),
+                                            Text(
+                                              '\$290.00',
+                                              style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      170, 20, 240, 1),
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.w500),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ],
