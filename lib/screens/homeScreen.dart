08153959924bdbf941ff096766a9be87f48d4add
adeenazara2002 <adinazara2001@gmail.com>
2024-08-16 00:56:45 +0500

    ecommerce app

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerceapp/widgets/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -50,47 +49,48 @@ class _HomeState extends State<Home> {
               ),
 
               // ==== PURPLE CONTAINER ====
-
-              Container(
-                margin: EdgeInsets.only(top: 30, left: 15, right: 15),
-                height: 150,
-                width: 360,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(170, 20, 240, 1),
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: Color.fromRGBO(170, 20, 240, 1),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-
-                      decoration: BoxDecoration(
-                        color: AppColors.heartColor,
-                        borderRadius: BorderRadius.circular(20),
-                        border: Border.all(
-                          color: AppColors.heartColor,
-                        ),
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30, left: 15, right: 15),
+                    height: 130,
+                    width: 360,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(170, 20, 240, 1),
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Color.fromRGBO(170, 20, 240, 1),
                       ),
                     ),
-
-                  // === container 2 ===
-                  Container(
-                      // width: MediaQuery.of(context).size.width,
-
-                      // decoration: BoxDecoration(
-                      //   color: AppColors.heartColor,
-                      //   borderRadius: BorderRadius.circular(20),
-                      //   border: Border.all(
-                      //     color: AppColors.heartColor,
-                      //   ),
-                      // ),
-                      child: Image.asset('./assets/images/shoe-bg.png'),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          decoration: BoxDecoration(
+                            color: AppColors.heartColor,
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: AppColors.heartColor,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  
+                  // Shoe Image Positioned above the container
+                  Positioned(
+                    top: -10, // Adjust this value to control how much the image floats above the container
+                    right: 15, // Adjust the horizontal position
+                    bottom: -20,
+                    // bottom: 20,
+                    child: Image.asset(
+                      './assets/images/shoe-bg.png',
+                      height: 100, // Adjust the size of the shoe image
+                      // width: 200,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
