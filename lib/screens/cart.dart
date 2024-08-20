8102b71cfdbf32233b904ed5fd219fe1a1dea00e
adeenazara2002 <adinazara2001@gmail.com>
2024-08-20 21:53:21 +0500

    ecommerce app

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -49,7 +49,7 @@ class _CartState extends State<Cart> {
                 ),
               ),
 
-              // ==== FIRST CONTAINER ====
+              // ==== STACK 1 ====
 
               Stack(
                 children: [
@@ -162,6 +162,418 @@ class _CartState extends State<Cart> {
                           ),
                           child: Image.asset('assets/images/deskwatch.png'),
                         )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // ==== STACK 2 ====
+
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 15, left: 15, right: 15),
+                    height: 100,
+                    width: 360,
+                    decoration: BoxDecoration(
+                      // color: Color.fromRGBO(170, 20, 240, 1),
+                      color: AppColors.primaryColor,
+
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        // color: Color.fromRGBO(170, 20, 240, 1),
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(left: 20, top: 30),
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Wooden Coffe Table',
+                                    style: TextStyle(
+                                      color: AppColors.homeColor,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 50)),
+                                  Icon(
+                                    Icons.add,
+                                    color: Color.fromRGBO(54, 52, 49, 1),
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Color.fromRGBO(255, 176, 1, 1),
+                                    size: 15,
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 5)),
+                                  Text(
+                                    '1273 reviews',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 89)),
+                                  Icon(
+                                    Icons.minimize_outlined,
+                                    color: Color.fromRGBO(54, 52, 49, 1),
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '\$290.00',
+                                    style: TextStyle(
+                                      color: AppColors.secondaryColor,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 120)),
+                                  Text(
+                                    '1',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(54, 52, 49, 1),
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        // ==== SECOND CONTAINER ====
+                        Padding(padding: EdgeInsets.only(left: 5)),
+                        Container(
+                          // padding: EdgeInsets.only(left: 50),
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            // color: Colors.red,
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Image.asset('assets/images/deskwatch.png'),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // ==== STACK 3 ====
+
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 15, left: 15, right: 15),
+                    height: 100,
+                    width: 360,
+                    decoration: BoxDecoration(
+                      // color: Color.fromRGBO(170, 20, 240, 1),
+                      color: AppColors.primaryColor,
+
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        // color: Color.fromRGBO(170, 20, 240, 1),
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(left: 20, top: 30),
+                          width: MediaQuery.of(context).size.width * 0.65,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Wooden Coffe Table',
+                                    style: TextStyle(
+                                      color: AppColors.homeColor,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 50)),
+                                  Icon(
+                                    Icons.add,
+                                    color: Color.fromRGBO(54, 52, 49, 1),
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.star,
+                                    color: Color.fromRGBO(255, 176, 1, 1),
+                                    size: 15,
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 5)),
+                                  Text(
+                                    '1273 reviews',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 89)),
+                                  Icon(
+                                    Icons.minimize_outlined,
+                                    color: Color.fromRGBO(54, 52, 49, 1),
+                                    size: 20,
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '\$290.00',
+                                    style: TextStyle(
+                                      color: AppColors.secondaryColor,
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 120)),
+                                  Text(
+                                    '1',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(54, 52, 49, 1),
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        // ==== SECOND CONTAINER ====
+                        Padding(padding: EdgeInsets.only(left: 5)),
+                        Container(
+                          // padding: EdgeInsets.only(left: 50),
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            // color: Colors.red,
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Image.asset('assets/images/deskwatch.png'),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              // ==== STACK 1 ====
+
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20, left: 15, right: 15),
+                    height: 150,
+                    width: 360,
+                    decoration: BoxDecoration(
+                      // color: Color.fromRGBO(170, 20, 240, 1),
+                      color: AppColors.primaryColor,
+
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        // color: Color.fromRGBO(170, 20, 240, 1),
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(left: 20, top: 10),
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Select Item',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 50)),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 40)),
+
+                                  Text(
+                                    'Subtotal:',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 89)),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 30)),
+
+                                  Text(
+                                    'Discount (%20):',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 40)),
+
+                                  Text(
+                                    'Total:',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        // ==== SECOND CONTAINER ====
+                        Padding(padding: EdgeInsets.only(left: 5)),
+                        Container(
+                          padding: EdgeInsets.only(left: 20, top: 10),
+                          // width: MediaQuery.of(context).size.width * 0.5,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'Select Item',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 50)),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 40)),
+
+                                  Text(
+                                    'Subtotal:',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Padding(padding: EdgeInsets.only(left: 89)),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 30)),
+
+                                  Text(
+                                    'Discount (%20):',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              Row(
+                                children: [
+                                  Padding(padding: EdgeInsets.only(top: 40)),
+
+                                  Text(
+                                    'Total:',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(154, 153, 152, 1),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
